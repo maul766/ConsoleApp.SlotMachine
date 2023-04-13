721e96b703de1ac4f9ff2c99973c23fa3c528a4e
Maul766 <maulanafaisal.7666@gmail.com>
2023-04-13 13:33:29 +0700

    Add project files.

---
+++ Gambit/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void Menu()
+{
+    std::cout << "\t\t______________________________________________\n";
+    std::cout << "\t\t|                                            |\n";
+    std::cout << "\t\t|             Welcome to Dead Gambit         |\n";
+    std::cout << "\t\t----------------------------------------------\n";
+}
+int main()
+{
+    Menu();
+    return 0;
+}
